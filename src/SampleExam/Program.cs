ab85586eb892ee49cb7fc76a24167515d01ea9de
Namig Hajiyev <namiq886@gmail.com>
2019-09-02 17:05:24 +0400

    add logging support

--- src/SampleExam/Program.cs
+++ src/SampleExam/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Settings.Configuration;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace SampleExam
 {
@@ -14,11 +18,33 @@ namespace SampleExam
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+
+            Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Debug()
+           .Enrich.FromLogContext()
+           .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] {SourceContext} {Message}{NewLine}{Properties}{NewLine}{Exception}"
+           , theme: AnsiConsoleTheme.Code)
+           .CreateLogger();
+
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog();
     }
 }
